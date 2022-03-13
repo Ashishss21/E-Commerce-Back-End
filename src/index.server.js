babfc350219021f54c5055d2329eda0cc98487fb
ashishss21 <ashish.sengar18@yahoo.com>
2022-03-13 22:28:39 +0530

    Bodyparser Added

--- src/index.server.js
+++ src/index.server.js
@@ -1,1 +1,23 @@
 const express = require('express');
+const env = require('dotenv');
+
+const app = express();
+const bodyParser = require('body-parser')
+
+// calling ENV variables
+env.config();
+
+app.use(bodyParser());
+
+let PORT = process.env.PORT
+
+// Call env file to use creds
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
+
+// Default Route
+app.get('/',(req,res,next)=>{
+    res.status(200);
+    res.send("Hello from get request");
+});
