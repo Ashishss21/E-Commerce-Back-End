3a7a66d8d698981a9c941f734083545680b04206
ashishss21 <ashish.sengar18@yahoo.com>
2022-03-14 23:39:30 +0530

    Mongoose Connection Error

--- src/index.server.js
+++ src/index.server.js
@@ -2,7 +2,22 @@ const express = require('express');
 const env = require('dotenv');
 
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+
+// DB-CONNECT
+DB_USERNAME = process.env.DB_USERNAME
+DB_PASSWORD = process.env.DB_PASSWORD
+DB_DATABASE = process.env.DB_DATABASE
+
+mongoose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@e-commerce-db.qhacu.mongodb.net/${DB_DATABASE}?retryWrites=true&w=majority`)
+.then(()=>{
+    console.log("Database Connected");
+})
+.catch((err)=>{
+    console.error(err);
+})
 
 // calling ENV variables
 env.config();
@@ -21,3 +36,9 @@ app.get('/',(req,res,next)=>{
     res.status(200);
     res.send("Hello from get request");
 });
+
+// Default Post Request
+app.post('/',(req,res,next)=>{
+    res.status(200);
+    res.send(`Hello ${req.body}`);
+})
